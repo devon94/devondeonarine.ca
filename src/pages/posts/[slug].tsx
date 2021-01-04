31d43e54ac7ff7a65e0e97d0142eed1f3428d043
Devon Deonarine <hello@devondeonarine.ca>
2021-01-03 21:46:05 -0500

    add amplify stuff and fix some build errors

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,7 +1,3 @@
-import Container from "@devondeonarine/components/container"
-import Header from "@devondeonarine/components/header"
-import Layout from "@devondeonarine/components/layout"
-import { CMS_NAME } from "@devondeonarine/lib/constants"
 import { useRouter } from "next/dist/client/router"
 import Head from "next/head"
 import React from "react"
@@ -15,77 +11,77 @@ interface Props {
 const Post: React.FunctionComponent<Props>  = (props) => {
   const { post, morePosts, preview } = props
   const router = useRouter()
-  
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
+  return null
+  // if (!router.isFallback && !post?.slug) {
+  //   return <ErrorPage statusCode={404} />
+  // }
 
-  return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
-    </Layout>
-  )
+  // return (
+  //   <Layout preview={preview}>
+  //     <Container>
+  //       <Header />
+  //       {router.isFallback ? (
+  //         <PostTitle>Loading…</PostTitle>
+  //       ) : (
+  //         <>
+  //           <article className="mb-32">
+  //             <Head>
+  //               <title>
+  //                 {post.title} | Next.js Blog Example with {CMS_NAME}
+  //               </title>
+  //               <meta property="og:image" content={post.ogImage.url} />
+  //             </Head>
+  //             <PostHeader
+  //               title={post.title}
+  //               coverImage={post.coverImage}
+  //               date={post.date}
+  //               author={post.author}
+  //             />
+  //             <PostBody content={post.content} />
+  //           </article>
+  //         </>
+  //       )}
+  //     </Container>
+  //   </Layout>
+  // )
 }
 
-export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+// export async function getStaticProps({ params }) {
+//   const post = getPostBySlug(params.slug, [
+//     'title',
+//     'date',
+//     'slug',
+//     'author',
+//     'content',
+//     'ogImage',
+//     'coverImage',
+//   ])
+//   const content = await markdownToHtml(post.content || '')
 
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  }
-}
+//   return {
+//     props: {
+//       post: {
+//         ...post,
+//         content,
+//       },
+//     },
+//   }
+// }
 
-export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
+// export async function getStaticPaths() {
+//   const posts = getAllPosts(['slug'])
 
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      }
-    }),
-    fallback: false,
-  }
-}
+//   return {
+//     paths: posts.map((post) => {
+//       return {
+//         params: {
+//           slug: post.slug,
+//         },
+//       }
+//     }),
+//     fallback: false,
+//   }
+// }
 
 
 export default Post
